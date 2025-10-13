2a1d2f5953692a0a229694fbda09f63a74426f89
Nguyen Thi Kim Oanh <oanhoanh2004/vn@gmail.com@example.com>
2025-10-13 22:14:28 +0700

    Xoá thư mục CNPM khỏi nhánh main

--- CNPM/src/main/java/nvkho/dto/request/ChiTietXuatKhoRequest.java
+++
@@ -1,9 +0,0 @@
-package nvkho.dto.request;
-
-import lombok.Data;
-
-@Data
-public class ChiTietXuatKhoRequest {
-    private String maSP;
-    private Integer soLuong;
-}
--- CNPM/src/main/java/nvkho/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nvkho.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
